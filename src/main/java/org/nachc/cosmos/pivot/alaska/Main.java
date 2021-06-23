6f07dd9eae465d8ba7be7e5bbbc33e6418fd73c4
unknown <greshje.nachc@gmail.com>
2021-06-23 14:17:52 -0400

    initial commit

---
+++ src/main/java/org/nachc/cosmos/pivot/alaska/Main.java
@@ -0,0 +1,13 @@
+package org.nachc.cosmos.pivot.alaska;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class Main {
+
+	public static void main(String[] args) {
+		log.info("Starting pivot...");
+		log.info("Done.");
+	}
+	
+}
