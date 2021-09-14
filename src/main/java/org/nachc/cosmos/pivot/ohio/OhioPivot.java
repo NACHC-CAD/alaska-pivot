638753e5c7903d58f4c5135eadfc4d90a7565b71
unknown <greshje.nachc@gmail.com>
2021-09-13 21:55:57 -0400

    adding ohio, work in progress, looks good

--- src/main/java/org/nachc/cosmos/pivot/ohio/OhioPivot.java
+++ src/main/java/org/nachc/cosmos/pivot/ohio/OhioPivot.java
@@ -22,7 +22,7 @@ public class OhioPivot {
 		new PivotUtil("-PREG", "-PREG_PIVOT", 20, 23, 4, 1, 20, headers).exec(srcFile);
 		// dx
 		headers = new String[] { "Zip", "Sex at Birth", "Race Unscrubbed", "Usual Provider", "Race", "Ethnicity", "Language", "Housing Situation UDS", "Fake Id", "Age", "Language Record Date", "Language Patient Detail", "FPL Range", "FPL Value", "Housing Trigger", "Insurance Financial Class", "Insurance Primary Payer", "Most Recent Encounter Date", "Most Recent Encounter Provider", "Most Recent Encounter Location", "Dx Date", "Dx Code", "Dx Category", "pos" };
-		new PivotUtil("-DX", "-DX_PIVOT", 24, 29, 2, 3, 20, headers).exec(srcFile, PivotType.ADD_HEADERS, 1);
+		new PivotUtil("-DX1", "-DX1_PIVOT", 24, 29, 2, 3, 20, headers).exec(srcFile, PivotType.ADD_HEADERS, 1);
 		// drug-ass
 		headers = new String[] { "Zip", "Sex at Birth", "Race Unscrubbed", "Usual Provider", "Race", "Ethnicity", "Language", "Housing Situation UDS", "Fake Id", "Age", "Language Record Date", "Language Patient Detail", "FPL Range", "FPL Value", "Housing Trigger", "Insurance Financial Class", "Insurance Primary Payer", "Most Recent Encounter Date", "Most Recent Encounter Provider", "Most Recent Encounter Location", "Drug Assessment Date", "Drug Assessment Code", "Drug Assessment Score", "Drug Use Status Date", "Drug Use Status Detail", "pos" };
 		new PivotUtil("-DRUGS", "-DRUGS_PIVOT", 30, 34, 5, 1, 20, headers).exec(srcFile);
@@ -35,9 +35,24 @@ public class OhioPivot {
 		// demo
 		headers = new String[] { "Zip", "Sex at Birth", "Race Unscrubbed", "Usual Provider", "Race", "Ethnicity", "Language", "Housing Situation UDS", "Fake Id", "Age", "Language Record Date", "Language Patient Detail", "FPL Range", "FPL Value", "Housing Trigger", "Insurance Financial Class", "Insurance Primary Payer", "Most Recent Encounter Date", "Most Recent Encounter Provider", "Most Recent Encounter Location", "Sexual Orientation", "Gender Identity", "pos" };
 		new PivotUtil("-DEMO", "-DEMO_PIVOT", 38, 39, 2, 1, 20, headers).exec(srcFile);
-		// rx1
+		// lab
 		headers = new String[] { "Zip", "Sex at Birth", "Race Unscrubbed", "Usual Provider", "Race", "Ethnicity", "Language", "Housing Situation UDS", "Fake Id", "Age", "Language Record Date", "Language Patient Detail", "FPL Range", "FPL Value", "Housing Trigger", "Insurance Financial Class", "Insurance Primary Payer", "Most Recent Encounter Date", "Most Recent Encounter Provider", "Most Recent Encounter Location", "Lab Date", "Lab Result", "Lab Category", "pos" };
-		new PivotUtil("-LAB", "-LAB_PIVOT", 40, 51, 2, 6, 20, headers).exec(srcFile, PivotType.ADD_HEADERS);
+		new PivotUtil("-LAB1", "-LAB1_PIVOT", 40, 51, 2, 6, 20, headers).exec(srcFile, PivotType.ADD_HEADERS);
+		// rx2
+		headers = new String[] { "Zip", "Sex at Birth", "Race Unscrubbed", "Usual Provider", "Race", "Ethnicity", "Language", "Housing Situation UDS", "Fake Id", "Age", "Language Record Date", "Language Patient Detail", "FPL Range", "FPL Value", "Housing Trigger", "Insurance Financial Class", "Insurance Primary Payer", "Most Recent Encounter Date", "Most Recent Encounter Provider", "Most Recent Encounter Location", "Rx Start Date", "Rx Stop Date", "Rx Name", "Rx Category", "pos" };
+		new PivotUtil("-RX2", "-RX2_PIVOT", 52, 54, 3, 1, 20, headers).exec(srcFile, PivotType.ADD_HEADERS, 2);
+		// lab2
+		headers = new String[] { "Zip", "Sex at Birth", "Race Unscrubbed", "Usual Provider", "Race", "Ethnicity", "Language", "Housing Situation UDS", "Fake Id", "Age", "Language Record Date", "Language Patient Detail", "FPL Range", "FPL Value", "Housing Trigger", "Insurance Financial Class", "Insurance Primary Payer", "Most Recent Encounter Date", "Most Recent Encounter Provider", "Most Recent Encounter Location", "Lab Date", "Lab Result", "Lab Category", "pos" };
+		new PivotUtil("-LAB2", "-LAB2_PIVOT", 55, 56, 2, 1, 20, headers).exec(srcFile, PivotType.ADD_HEADERS, 1);
+		// enc
+		headers = new String[] { "Zip", "Sex at Birth", "Race Unscrubbed", "Usual Provider", "Race", "Ethnicity", "Language", "Housing Situation UDS", "Fake Id", "Age", "Language Record Date", "Language Patient Detail", "FPL Range", "FPL Value", "Housing Trigger", "Insurance Financial Class", "Insurance Primary Payer", "Most Recent Encounter Date", "Most Recent Encounter Provider", "Most Recent Encounter Location", "Primary Care Encounter Date", "Primary Care Encounter Provider", "Primary Care Encounter Location", "Primary Care Encounter Count", "pos" };
+		new PivotUtil("-ENC", "-ENC_PIVOT", 57, 60, 4, 1, 20, headers).exec(srcFile);
+		// lab3
+		headers = new String[] { "Zip", "Sex at Birth", "Race Unscrubbed", "Usual Provider", "Race", "Ethnicity", "Language", "Housing Situation UDS", "Fake Id", "Age", "Language Record Date", "Language Patient Detail", "FPL Range", "FPL Value", "Housing Trigger", "Insurance Financial Class", "Insurance Primary Payer", "Most Recent Encounter Date", "Most Recent Encounter Provider", "Most Recent Encounter Location", "Lab Date", "Lab Code", "Lab Result", "Lab Category", "pos" };
+		new PivotUtil("-LAB3", "-LAB3_PIVOT", 61, 63, 3, 1, 20, headers).exec(srcFile, PivotType.ADD_HEADERS, 2);
+		// hx
+		headers = new String[] { "Zip", "Sex at Birth", "Race Unscrubbed", "Usual Provider", "Race", "Ethnicity", "Language", "Housing Situation UDS", "Fake Id", "Age", "Language Record Date", "Language Patient Detail", "FPL Range", "FPL Value", "Housing Trigger", "Insurance Financial Class", "Insurance Primary Payer", "Most Recent Encounter Date", "Most Recent Encounter Provider", "Most Recent Encounter Location", "Hx Date", "Hx Detail", "Hx Category", "pos" };
+		new PivotUtil("-HX", "-HX_PIVOT", 64, 77, 2, 7, 20, headers).exec(srcFile, PivotType.ADD_HEADERS, 1);
 		// PARAMETERS: flatSuffix, String pivotSuffix, int startFlat, int endFlat, int pivotWidth, int pivotRepeat, int constantsWidth, Object[] pivotHeaders
 		// done
 		log.info("Done.");
