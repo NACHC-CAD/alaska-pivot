3856ff9e097a7008cfe0d3bd7f6db73f4f4d6e11
unknown <greshje.nachc@gmail.com>
2021-11-01 09:25:27 -0400

    work in progress, looks good

---
+++ src/main/java/org/nachc/cosmos/pivot/util/pivot/Pivot02HeaderPivot.java
@@ -0,0 +1,16 @@
+package org.nachc.cosmos.pivot.util.pivot;
+
+import java.io.File;
+
+/**
+ * 
+ * This pivot takes the 
+ *
+ */
+public class Pivot02HeaderPivot {
+
+	public void exec(File src, File flat, File pivot, String[] colNames, Integer[] cols) {
+		
+	}
+	
+}
